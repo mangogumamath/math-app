297c2ee3aa1ab4d97731edfac4052a50cb1fa355
mangogumamath <mangogumamath@gmail.com>
2021-11-28 00:45:28 +0900

    최고점수 저장 추가

--- lib/screens/my_screen.dart
+++ lib/screens/my_screen.dart
@@ -48,10 +48,10 @@ class _MyScreenState extends State<MyScreen> {
           // ElevatedButton(
           //     onPressed: _removeScore, child: FaIcon(FontAwesomeIcons.eraser)),
           // Text('로그인'),
-          Text(
-            '본 앱은 정수와 유리수의 계산을 연습하기 위해 만들어졌습니다.',
-            style: TextStyle(fontSize: 40.0),
-          ),
+          // Text(
+          //   '본 앱은 정수와 유리수의 계산을 연습하기 위해 만들어졌습니다.',
+          //   style: TextStyle(fontSize: 40.0),
+          // ),
           // Text(
           //   '기능 준비중',
           //   style: TextStyle(fontSize: 40.0),
